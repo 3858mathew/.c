0b77979bd1586ad91bcbf2cedfbd5806c923104c
3858mathew <101691229+3858mathew@users.noreply.github.com>
2022-03-28 19:07:09 +0300

    Add files via upload

---
+++ Mathews sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=12;
+    int b=13;
+    int c=14;
+    int d=15;
+    int e=16;
+    int sum;
+
+    sum=a+b+c+d+e;
+
+    printf("sum of five numbers %d\n");
+    return 0;
+}
